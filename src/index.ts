bcaead4630a7fc1932bbe68e7da2ba0a806d19bc
Anthony Fu <anthonyfu117@hotmail.com>
2021-08-24 17:17:46 +0800

    chore: init

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import { createUnplugin } from 'unplugin'
+import { Options } from './types'
+
+export default createUnplugin<Options>(options => ({
+  name: 'unplugin-starter',
+  enforce: 'post',
+  transformInclude(id) {
+    return id.endsWith('main.ts')
+  },
+  transform(code) {
+    return code.replace('__UNPLUGIN__', `Hello Unplugin! ${options}`)
+  },
+}))
---
+++ test/index.test.ts
@@ -0,0 +1,3 @@
+describe('index', () => {
+  expect(1).toBe(1)
+})
