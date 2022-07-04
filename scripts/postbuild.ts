cc356b11d454737b3c61d3a7abacb54f6f0d1018
Anthony Fu <anthonyfu117@hotmail.com>
2022-07-04 22:27:56 +0800

    chore: lint

--- scripts/postbuild.ts
+++ scripts/postbuild.ts
@@ -10,7 +10,6 @@ async function run() {
     cwd: resolve(__dirname, '../dist'),
   })
   for (const file of files) {
-    // eslint-disable-next-line no-console
     console.log(chalk.cyan.inverse(' POST '), `Fix ${basename(file)}`)
     // fix cjs exports
     let code = await fs.readFile(file, 'utf8')
