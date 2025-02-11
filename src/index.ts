c5a911f42d9e5b46c9435d586d2475b8859056e7
Anthony Fu <github@antfu.me>
2025-02-11 16:06:44 +0800

    chore: update deps

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import type { UnpluginFactory } from 'unplugin'
-import { createUnplugin } from 'unplugin'
 import type { Options } from './types'
+import { createUnplugin } from 'unplugin'
 
 export const unpluginFactory: UnpluginFactory<Options | undefined> = options => ({
   name: 'unplugin-starter',
--- src/nuxt.ts
+++ src/nuxt.ts
@@ -1,7 +1,7 @@
+import type { Options } from './types'
 import { addVitePlugin, addWebpackPlugin, defineNuxtModule } from '@nuxt/kit'
 import vite from './vite'
 import webpack from './webpack'
-import type { Options } from './types'
 import '@nuxt/schema'
 
 export interface ModuleOptions extends Options {
